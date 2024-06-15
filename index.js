1501ed77301a6572f4ac00ab782ff199a1d03af0
SheikhMujib <mbasheikhmujib@gmail.com>
2024-06-15 23:04:11 +0600

    Vercel file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -23,9 +24,21 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    app.get('jwt', async(req, res) =>{
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        return res.send({ accessToken: token });
+    }
+    res.status(403).send({ accessToken: '' })
+    })
+
     //users info collection
     app.post('/users', async(req, res) =>{
       const user = req.body;
+      console.log(user);
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
